c5d54edafcf31defc3fa37b05046f6f7c5c75cd9
Sathya <neelasathya06@gmail.com>
2025-03-26 21:04:21 -0400

    Ecommerce Application CRUDs

---
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -0,0 +1,16 @@
+package com.ecommerce.project.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter@Setter
+public class Category {
+
+    private Long categoryId;
+    private String categoryName;
+
+
+}
