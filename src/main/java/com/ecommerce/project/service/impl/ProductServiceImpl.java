8edb6f5915981b4e7efff03afeae042567d66bdf
Sathya <neelasathya06@gmail.com>
2025-04-06 17:53:58 -0400

    153. Use of Fileservice and application.properties

--- src/main/java/com/ecommerce/project/service/impl/ProductServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/impl/ProductServiceImpl.java
@@ -7,9 +7,13 @@ import com.ecommerce.project.payload.ProductDTO;
 import com.ecommerce.project.payload.ProductResponse;
 import com.ecommerce.project.repository.CategoryRepository;
 import com.ecommerce.project.repository.ProductRepository;
+import com.ecommerce.project.service.FileService;
 import com.ecommerce.project.service.ProductService;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -28,6 +32,10 @@ public class ProductServiceImpl implements ProductService {
     private final ProductRepository productRepository;
     private final CategoryRepository categoryRepository;
     private ModelMapper modelMapper;
+    private FileService fileService;
+
+    @Value("${project.image}")
+    private String path;
 
     @Override
     public ProductDTO addProduct(Long categoryId, ProductDTO productDTO) {
@@ -124,8 +132,8 @@ public class ProductServiceImpl implements ProductService {
         Product productFromDb = productRepository.findById(productId)
                 .orElseThrow(() -> new ResourceNotFoundException("Product", "productId", productId));
 
-        String path = "images/";
-        String fileName = uploadImage(path, image);
+//        String path = "images/";
+        String fileName = fileService.uploadImage(path, image);
 
         productFromDb.setImage(fileName);
 
@@ -134,27 +142,5 @@ public class ProductServiceImpl implements ProductService {
         return modelMapper.map(updateProduct, ProductDTO.class);
     }
 
-    private String uploadImage(String path, MultipartFile file) throws IOException {
 
-//        File Names of current / original file
-        String originalFileName = file.getOriginalFilename();
-
-//        Generate a unique file name
-        String randomId = UUID.randomUUID().toString();
-        String fileName = randomId.concat(originalFileName.substring(originalFileName.lastIndexOf(".")));
-        String filePath = path + File.separator + fileName;
-
-//        Check if path exists and create
-        File folder = new File(path);
-        if(!folder.exists()){
-            folder.mkdirs();
-        }
-
-//        upload to server
-        Files.copy(file.getInputStream(), Paths.get(filePath));
-
-//        return file name
-        return fileName;
-
-    }
 }
