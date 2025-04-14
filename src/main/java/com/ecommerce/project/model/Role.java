fb4cf7f3e86fc2937689b0f2b657167622a4b0f7
Sathya <neelasathya06@gmail.com>
2025-04-14 13:56:11 -0400

    183. Implemented users, address, and roles

---
+++ src/main/java/com/ecommerce/project/model/Role.java
@@ -0,0 +1,28 @@
+package com.ecommerce.project.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@NoArgsConstructor
+@Getter@Setter@ToString
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Integer roleId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20, name = "role_name")
+    private AppRole roleName;
+
+    public Role(AppRole roleName) {
+        this.roleName = roleName;
+    }
+
+}
